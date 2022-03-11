9bc5e1367f72d36c43f6054b0629145989183b2e
unknown <carsonnordmann1210@gmail.com>
2022-03-11 11:50:00 -0500

    Subtraction

--- SoftwareEngineering.java
+++ SoftwareEngineering.java
@@ -4,11 +4,17 @@ public class SoftwareEngineering {
     public static void main (String args[]) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("1. Addition\n2. Subtraction\nPick and option:");
+        int menuOption = scanner.nextInt();
+
         System.out.print("First number: ");
         int value1 = scanner.nextInt();
         System.out.print("Second number: ");
         int value2 = scanner.nextInt();
 
-        System.out.print("Sum: " + (value1 + value2));
+        if (menuOption == 1)
+            System.out.print("Sum: " + (value1 + value2));
+        else if (menuOption == 2)
+            System.out.print("Difference: " + (value1 - value2));
     }
 }
